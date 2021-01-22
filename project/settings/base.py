e58c476973ced7303f4b2b25e843ff9025fce4fe
ri003074 <ri003074@gmail.com>
2021-01-22 23:54:20 +0900

    rm

--- project/settings/base.py
+++ project/settings/base.py
@@ -159,10 +159,3 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
 
 STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, "static"),)
-
-import django_heroku
-from .base import *  # noqa
-
-DEBUG = True  # should be false but for debug
-
-django_heroku.settings(locals())
