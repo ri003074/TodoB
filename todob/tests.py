2bfe4147f575afba73a4e375bcb7d8bb18248734
ri003074 <ri003074@gmail.com>
2021-01-22 18:46:17 +0900

    add task model test

--- todob/tests.py
+++ todob/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Task
 
-# Create your tests here.
+
+class TaskModelTest(TestCase):
+    def setUp(self):
+        Task.objects.create(title="abc")
+
+    def test_task(self):
+        task_title = Task.objects.get(title="abc")
+
+        self.assertEqual(task_title.title, "abc")
