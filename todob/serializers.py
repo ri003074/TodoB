6967116fcdf9117a3eaa53f19e843c80f71d238e
ri003074 <ri003074@gmail.com>
2021-01-23 11:27:17 +0900

    add foreignkey

--- todob/serializers.py
+++ todob/serializers.py
@@ -1,8 +1,30 @@
 from rest_framework import serializers
 from .models import Task
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username")
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    user_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), write_only=True
+    )
+
+    def create(self, validated_date):
+        validated_date["user"] = validated_date.get("user_id", None)
+
+        if validated_date["user"] is None:
+            raise serializers.ValidationError("user not found.")
+
+        del validated_date["user_id"]
+
+        return Task.objects.create(**validated_date)
+
     class Meta:
         model = Task
-        fields = ["id", "title"]
+        fields = ("id", "title", "user", "user_id")
--- todob/views.py
+++ todob/views.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .models import Task
 from .serializers import TaskSerializer
-
-# Create your views here.
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
