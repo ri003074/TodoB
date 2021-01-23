5ca7613a010d3ac982ba9a43bb49c027cf9ac3d6
ri003074 <ri003074@gmail.com>
2021-01-23 12:40:34 +0900

    fix test

--- todob/tests.py
+++ todob/tests.py
@@ -1,10 +1,13 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from .models import Task
 
 
 class TaskModelTest(TestCase):
     def setUp(self):
-        Task.objects.create(title="abc")
+        user = User()
+        user.save()
+        Task.objects.create(title="abc", user=user)
 
     def test_task(self):
         task_title = Task.objects.get(title="abc")
