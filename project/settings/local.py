396eba3dd06a25ec380958df03fa60b347ee6015
ri003074 <ri003074@gmail.com>
2021-01-24 13:01:33 +0900

    add logging setup

--- project/settings/local.py
+++ project/settings/local.py
@@ -1,5 +1,11 @@
 from .base import *  # noqa
+import logging
 
 DEBUG = True
 
 ACCOUNT_EMAIL_VERIFICATION = "none"
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(message)s",
+)
