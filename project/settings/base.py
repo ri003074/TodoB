dd0036930b76fa9b37702828a9aa96a5cd192d59
ri003074 <ri003074@gmail.com>
2021-01-24 09:27:35 +0900

    add emailsetting

--- project/settings/base.py
+++ project/settings/base.py
@@ -159,3 +159,5 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
 
 STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, "static"),)
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
