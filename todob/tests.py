37ca013ff7de46fc0efb04763d6705be7c2d5faa
ri003074 <ri003074@gmail.com>
2021-01-27 22:34:13 +0900

    add   taskviewset test    code

--- todob/tests.py
+++ todob/tests.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import User
+from django.test import RequestFactory
 from django.test import TestCase
+from rest_framework.test import force_authenticate
 from .models import Task
+from .views import TaskViewSet
 
 
 class TaskModelTest(TestCase):
@@ -13,3 +16,26 @@ class TaskModelTest(TestCase):
         task_title = Task.objects.get(title="abc")
 
         self.assertEqual(task_title.title, "abc")
+
+
+class TestTaskViewSet(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_superuser(
+            username="kenta", password="kawamoto", email="kenta@gmail.com"
+        )
+        Task.objects.create(title="task1-kenta", user=self.user)
+        Task.objects.create(title="task2-kenta", user=self.user)
+        Task.objects.create(title="task3-kenta", user=self.user)
+
+    def test_task_viewset(self):
+        request = self.factory.get("/api/tasks/")
+        force_authenticate(request, user=self.user)
+        response = TaskViewSet.as_view({"get": "list"})(request)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data[0]["id"], 1)
+        self.assertEqual(response.data[0]["title"], "task1-kenta")
+        self.assertEqual(response.data[0]["user"]["id"], 1)
+        self.assertEqual(response.data[0]["user"]["username"], "kenta")
+        self.assertEqual(response.data[0]["is_done"], False)
